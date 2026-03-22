9851aa685a30740c55a66b2083952f70f4765a52
rotanpuolikas <tuukka.alatalo@gmail.com>
2026-03-22 20:22:48 +0200

    placeholder themes, login screen and navigation screen from previous project

---
+++ mobileBudgeting/screens/Login.js
@@ -0,0 +1,69 @@
+import { useState, useContext } from "react"
+import { View, Text, TextInput, TouchableOpacity, ActivityIndicator, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback, Platform } from "react-native";
+import { AuthContext } from "../context/AuthContext"
+import { styles } from "../theme/Theme.js"
+import { colours } from "../theme/Colours.js"
+
+const LoginScreen = () => {
+  const { login } = useContext(AuthContext)
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  const handleLogin = async () => {
+    setError("")
+    setLoading(true)
+
+    try {
+      await login(email, password);
+    }
+    catch (err) {
+      setError("Invalid email or password")
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <KeyboardAvoidingView style={styles.loginContainer} keyboardVerticalOffset={null} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Text style={styles.title}>Welcome Back</Text>
+
+          <TextInput
+            placeholder="Email"
+            placeholderTextColor={colours.textSecondary}
+            value={email}
+            onChangeText={setEmail}
+            style={styles.input}
+            autoCapitalize="none"
+            inputMode="email"
+          />
+
+          <TextInput
+            placeholder="Password"
+            placeholderTextColor={colours.textSecondary}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            style={styles.input}
+          />
+
+          {error ? <Text style={styles.error}>{error}</Text> : null}
+
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            {loading ? (
+              <ActivityIndicator color={colours.whiteText} />
+            ) : (
+              <Text style={styles.buttonText}>Login</Text>
+            )}
+          </TouchableOpacity>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  )
+}
+
+export default LoginScreen
---
+++ mobileBudgeting/theme/Theme.js
@@ -0,0 +1,41 @@
+import { colours } from "./colours.js"
+
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    backgroundColor: colours.background,
+    padding: 20,
+    justifyContent: "center",
+  },
+  input: {
+    backgroundColor: colours.card,
+    padding: 14,
+    borderRadius: 12,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: colours.borderColour,
+  },
+  button: {
+    backgroundColor: colours.primary,
+    padding: 15,
+    borderRadius: 12,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: colours.whiteText,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  error: {
+    color: colours.error,
+    padding: 5,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: colours.textPrimary,
+    marginBottom: 5,
+  },
+})
